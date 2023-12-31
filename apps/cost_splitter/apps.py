4baaed7532fedee5178d94feb3d6c215f1cf4a5c
Robert Nyström <robert.nystrom@toolsense.io>
2023-12-31 13:27:20 +0100

    add app

---
+++ apps/cost_splitter/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class CostSplitterConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.cost_splitter"
+    verbose_name = "Cost Splitter"
