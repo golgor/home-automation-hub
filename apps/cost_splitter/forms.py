4b7f390dd08f559e86aed5ad8c7a55ad52bf76ac
Robert Nyström <robert.nystrom@toolsense.io>
2023-12-31 22:18:53 +0100

    form to add CostSplitReport

--- apps/cost_splitter/forms.py
+++ apps/cost_splitter/forms.py
@@ -1,6 +1,6 @@
-from django.forms import ModelForm
+from django.forms import CheckboxSelectMultiple, ModelForm, MultipleChoiceField
 
-from .models import Cost
+from .models import Cost, CostSplitReport
 
 
 class AddCostForm(ModelForm):
@@ -11,3 +11,25 @@ class AddCostForm(ModelForm):
 
         model = Cost
         fields = ["location", "user", "date", "amount", "excluded_amount", "description"]
+
+
+class AddReportForm(ModelForm):
+    """Form used to add a new report."""
+
+    cost_list = MultipleChoiceField(required=False, widget=CheckboxSelectMultiple)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        unmanaged_costs_items = Cost.objects.filter(included_in_report=None).values("id", "location", "description")
+        unmanaged_costs = [
+            (cost_item["id"], f"{cost_item["location"]} - {cost_item["description"]}")
+            for cost_item in unmanaged_costs_items
+        ]
+        test = Cost.objects.filter(included_in_report=None).values_list("id", "location")
+        self.fields["cost_list"].choices = unmanaged_costs
+
+    class Meta:
+        """Meta class for AddReportForm."""
+
+        model = CostSplitReport
+        fields = ["date", "description"]
--- apps/cost_splitter/views.py
+++ apps/cost_splitter/views.py
@@ -4,7 +4,7 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 from django.views.generic.base import TemplateView, View
 
-from .forms import AddCostForm
+from .forms import AddCostForm, AddReportForm
 from .models import Cost, CostSplitReport
 
 
@@ -93,5 +93,30 @@ class AddCostFormView(View):
         form = AddCostForm(self.request.POST)
         if form.is_valid():
             form.save()
+
+        return redirect("cost_splitter:list_cost")
+
+
+class AddReportFormView(View):
+    """View to add a new report using a form."""
+
+    template_name = "add_report_form.html"
+
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any):
+        """GET method to get the form to add a new cost."""
+        form = AddReportForm()
+        context = {
+            "form": form,
+        }
+
+        return HttpResponse(render(self.request, self.template_name, context))
+
+    def post(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        """POST method to add a new cost."""
+        form = AddReportForm(self.request.POST)
+        if form.is_valid():
+            assigned_cost_items = form.cleaned_data["cost_list"]
+            form.save()
+            Cost.objects.filter(id__in=assigned_cost_items).update(included_in_report=form.instance)
 
         return redirect("cost_splitter:list_cost")
