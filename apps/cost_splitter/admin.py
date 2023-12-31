7ceebd3a23436c642718f1976f112be67a71e7b9
Robert Nyström <robert.nystrom@toolsense.io>
2023-12-31 15:56:24 +0100

    custom admin for monthly report

--- apps/cost_splitter/admin.py
+++ apps/cost_splitter/admin.py
@@ -1,3 +1,20 @@
+from __future__ import annotations
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cost, MonthlyReport
+
+
+class MonthlyReportAdmin(admin.ModelAdmin[MonthlyReport]):
+    """Custom admin class for MonthlyReport."""
+
+    resource_class = MonthlyReport
+    list_display = ["id", "name", "date", "summed_cost"]
+
+    def summed_cost(self, obj: MonthlyReport):
+        """Field to display the summed cost of all costs in a monthly report."""
+        return sum(obj.cost_set.select_related("cost_set").values_list("cost", flat=True))
+
+
+admin.site.register(Cost)
+admin.site.register(MonthlyReport, MonthlyReportAdmin)
--- apps/cost_splitter/apps.py
+++ apps/cost_splitter/apps.py
@@ -2,6 +2,8 @@ from django.apps import AppConfig
 
 
 class CostSplitterConfig(AppConfig):
+    """AppConfig for the cost_splitter app."""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.cost_splitter"
     verbose_name = "Cost Splitter"
