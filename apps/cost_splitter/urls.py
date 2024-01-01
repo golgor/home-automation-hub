e79309587cc01bbb257369504a6642b370bddabb
Robert Nyström <robert.nystrom@toolsense.io>
2024-01-01 11:54:29 +0100

    add urls for api

--- apps/cost_splitter/urls.py
+++ apps/cost_splitter/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 
+from . import api
 from .views import AddCostFormView, AddReportFormView, CostsListView, CostSplitReportListView, CostSplitReportView
+
+
+router = routers.DefaultRouter()
+router.register(r"costs", api.CostView, "cost")
+router.register(r"reports", api.CostSplitReportView, "report")
 
 
 urlpatterns = [
@@ -9,4 +16,5 @@ urlpatterns = [
     path("costs/", CostsListView.as_view(), name="list_cost"),
     path("add_cost/", AddCostFormView.as_view(), name="add_cost"),
     path("add_report/", AddReportFormView.as_view(), name="add_report"),
+    path("api/", include(router.urls)),
 ]
