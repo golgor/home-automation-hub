a5054f19aa4f9185e4e307a29fa6f55f0099b9c9
Robert Nyström <robert.nystrom@toolsense.io>
2024-01-01 22:31:21 +0100

    calculate cost split, add transcation in template

--- apps/cost_splitter/views.py
+++ apps/cost_splitter/views.py
@@ -1,6 +1,7 @@
 from typing import Any, TypedDict
 
 from django.contrib.auth import get_user_model
+from django.db.models import Sum
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 from django.views.generic.base import TemplateView, View
@@ -18,6 +19,47 @@ class MonthlyReportContext(TypedDict):
     id: int
 
 
+class Transaction(TypedDict):
+    """Type definition for a transaction."""
+
+    from_: str
+    to: str
+    amount: int
+
+
+def calculate_cost_split(report: CostSplitReport) -> list[Transaction]:
+    """Calculate the cost split for a monthly report."""
+    costs_per_user = (
+        Cost.objects.filter(included_in_report=report)
+        .values("user__id", "user__first_name")
+        .annotate(total=Sum("amount"))
+    )
+    total_cost = Cost.objects.filter(included_in_report=report).aggregate(Sum("amount"))
+    for user in costs_per_user:
+        user["diff"] = user["total"] - (total_cost["amount__sum"] / len(costs_per_user))
+    outstanding_differences = sorted(costs_per_user, key=lambda k: k["diff"], reverse=True)
+
+    transactions: list[Transaction] = []
+
+    while outstanding_differences:
+        highest_diff = outstanding_differences[0]
+        lowest_diff = outstanding_differences[-1]
+        transactions.append(
+            {
+                "to": highest_diff["user__first_name"],
+                "from_": lowest_diff["user__first_name"],
+                "amount": highest_diff["diff"],
+            }
+        )
+        highest_diff["diff"] += lowest_diff["diff"]
+        lowest_diff["diff"] = 0
+        if highest_diff["diff"] >= 0:
+            outstanding_differences.pop()
+        if lowest_diff["diff"] <= 0:
+            outstanding_differences.pop()
+    return transactions
+
+
 # Create your views here.
 class CostSplitReportView(TemplateView):
     """View to display a CostSplitReport."""
@@ -32,10 +74,16 @@ class CostSplitReportView(TemplateView):
             report = CostSplitReport.objects.get(id=context["id"])
         except CostSplitReport.DoesNotExist:
             return {"error": "Monthly report does not exist."}
+        try:
+            transactions = calculate_cost_split(report)
+        except Exception as e:
+            print(e)
+            transactions = []
 
         costs = Cost.objects.filter(included_in_report=report)
-
         context["costs"] = costs
+        context["transactions"] = transactions
+        context["report"] = report
         return context
 
 
