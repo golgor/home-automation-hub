0d83ad52a0203a5278cd80833373131306e30620
Robert Nyström <robert.nystrom@toolsense.io>
2024-01-03 20:54:29 +0100

    remove obsolete things in AddReportForm

--- apps/cost_splitter/forms.py
+++ apps/cost_splitter/forms.py
@@ -18,17 +18,7 @@ class AddCostForm(ModelForm):
 class AddReportForm(ModelForm):
     """Form used to add a new report."""
 
-    cost_list = MultipleChoiceField(required=False, widget=CheckboxSelectMultiple)
-
-    def __init__(self, *args: Any, **kwargs: Any):
-        """Initialize the form to enable dynamic choices for the cost_list field."""
-        super().__init__(*args, **kwargs)
-        unmanaged_costs_items = Cost.objects.filter(included_in_report=None).values("id", "location", "description")
-        unmanaged_costs = [
-            (cost_item["id"], f"{cost_item["location"]} - {cost_item["description"]}")
-            for cost_item in unmanaged_costs_items
-        ]
-        self.fields["cost_list"].choices = unmanaged_costs
+    cost_list = MultipleChoiceField(required=True)
 
     class Meta:
         """Meta class for AddReportForm."""
