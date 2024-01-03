6269fcf4b71d2c8705052e0c2bdb555a3f3c2529
Robert Nyström <robert.nystrom@toolsense.io>
2024-01-03 22:46:40 +0100

    fix using django_db

--- apps/cost_splitter/tests/test_utils.py
+++ apps/cost_splitter/tests/test_utils.py
@@ -1,8 +1,9 @@
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 
+import pytest
 from django.contrib.auth import get_user_model
 
-from apps.cost_splitter.models import Cost, CostSplitReport
+from apps.cost_splitter.models import CostSplitReport
 from apps.cost_splitter.types import Person
 from apps.cost_splitter.utils import get_total_expenses
 
@@ -10,9 +11,13 @@ from apps.cost_splitter.utils import get_total_expenses
 User = get_user_model()
 
 
+@pytest.mark.django_db
 def test_get_total_expenses():
-    # Create a mock CostSplitReport object
-    report = CostSplitReport()
+    """Test the get_total_expenses function.
+
+    This is testing only the dict comprehension part of the function. The rest is a query using django ORM.
+    """
+    report = CostSplitReport.objects.create(date="2021-01-01")
 
     # Create some mock Cost objects
     cost1 = {"user__id": 1, "user__first_name": "John", "total": 10}
@@ -24,10 +29,8 @@ def test_get_total_expenses():
         # Create a MagicMock for ORM operations
         mock_filter.return_value.values.return_value.annotate.return_value = [cost1, cost2, cost3]
 
-        # Call the function under test
         result = get_total_expenses(report)
 
-        # Assert the expected output
         assert result == {
             Person(person_id=1, name="John"): 10.0,
             Person(person_id=2, name="Jane"): 20.0,
